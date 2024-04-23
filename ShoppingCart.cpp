a3395cfa5fcdca0ca5d1538559d1c4ddcea2e267
cdekshenieks <167340770+cdekshenieks@users.noreply.github.com>
2024-04-23 10:24:52 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -6,4 +6,19 @@ using namespace std;
 ShoppingCart::ShoppingCart() : customerName("none"), currentDate("January 1, 2016") {}
 
 //Adding and Removing items from the vector
+void ShoppingCart::addItem(ItemToPurchase item) {
+  cartItems.push_back(item.GetName());
+  cartItems[itemQuantity] = item;
+  itemQuantity++;
+  totalPrice += item.GetPrice();
+}
 
+void ShoppingCart::removeItem(ItemToPurchase item) {
+  bool found = false;
+  while (int i = 0; i < itemCount && !found) {
+    if (cartItems[i].GetName() == item.GetName()) {
+      found = true;
+      cartItems.pop_back(item.GetName())
+      totalPrice -= cart[i].GetPrice();
+      itemQuantity--;
+      for 
