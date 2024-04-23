57db1670043cb3a9c1788fac4315142d3e4c8216
cdekshenieks <167340770+cdekshenieks@users.noreply.github.com>
2024-04-22 20:33:37 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+using namespace std;
+
+#include "ShoppingCart.h"
+
+ShoppingCart::ShoppingCart() : customerName("none"), currentDate("January 1, 2016") {}
+
+//Adding and Removing items from the vector
 
