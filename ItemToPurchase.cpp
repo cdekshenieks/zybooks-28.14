60e89b01ef5bd2381e2a1f0bec3803083f6a6623
savannahNelson04 <167340754+savannahNelson04@users.noreply.github.com>
2024-04-24 11:25:53 -0500

    Update ItemToPurchase.cpp
    created functions for ItemToPurchse.h

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -2,8 +2,39 @@
 #include <iostream>
 
 ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
-
+  this->name = name;
+  this->description = description;
+  this->price = price;
+  this->quantity = quantity;
 }
-void ItemToPurchase::
+ItemToPurchase::ItemToPurchase(){
+name = "";
+description = "none";
+price = 0;
+quantity = 0; 
+}
+
+//Setter Function
+void ItemToPurchase::SetDescription(string& description) {
+  this->description = description;
+}
+
+//Print functions
+
+void ItemToPurchase::PrintItemCost(){
+cout << name << " " << quantity << " @ $" << price << " = $" << quantity * price << endl;
+}
+void ItemToPurchase::PrintItemDescription(){
+cout << name << ": " << description << endl;
+}
+
+
+
+
+
+
+
+
+
 
 
