42ab806bca4410679062929d3e501c4ae9bbfdf5
cdekshenieks <167340770+cdekshenieks@users.noreply.github.com>
2024-04-24 11:27:58 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 ShoppingCart::ShoppingCart() : customerName("none"), currentDate("January 1, 2016") {}
 
+string GetCustomerName() {
+  return customerName;
+} 
+
+string GetDate() {
+  return currentDate;
+}
+
 //Adding and Removing items from the vector
-void ShoppingCart::addItem(ItemToPurchase item) {
+void ShoppingCart::addItem(ItemToPurchase item, ) {
   cartItems.push_back(item.GetName());
   cartItems[itemQuantity] = item;
   itemQuantity++;
@@ -15,10 +23,41 @@ void ShoppingCart::addItem(ItemToPurchase item) {
 
 void ShoppingCart::removeItem(ItemToPurchase item) {
   bool found = false;
-  while (int i = 0; i < itemCount && !found) {
+  while (int i = 0; i < cartItems.length && !found) {
     if (cartItems[i].GetName() == item.GetName()) {
       found = true;
       cartItems.pop_back(item.GetName())
       totalPrice -= cart[i].GetPrice();
       itemQuantity--;
-      for 
+      for (int j = i; j < itemCount; j++) {
+                cart[j] = cart[j + 1];
+            }
+        }
+        i++;
+    }
+    if (!found) {
+        cout << "Item not found in cart." << endl;
+    }
+}
+
+//define ModifyItem function
+void ModifyItem(ItemToPurchase item) {
+  for (int i = 0; i < cartItems.length; i++) {
+    if (cartItems[i].GetName() == item.GetName()) {
+
+
+int GetItemsInCart() {
+  return cartItems[];
+}
+
+double GetCostOfCart() {
+  return
+
+
+
+
+
+
+
+
+
