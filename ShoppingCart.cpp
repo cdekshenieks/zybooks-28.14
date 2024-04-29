e101ed05cefb19a7d8aa79d8edb516d9fc6bc66d
savannahNelson04 <167340754+savannahNelson04@users.noreply.github.com>
2024-04-29 15:32:45 -0500

    Update ShoppingCart.cpp
    Finished

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -34,15 +34,15 @@ void ShoppingCart::AddItem(ItemToPurchase item) {
 void ShoppingCart:: RemoveItem(string item) {
    bool found = false;
     for (unsigned int i = 0; i < cartItems.size(); i++) { 
-      if (item == cartItems.at(i).GetName()) { 
-         cartItems.erase(cartItems.begin()+i); 
-         found = true; 
-         break; 
-      } 
+       if (item == cartItems.at(i).GetName()) { 
+          cartItems.erase(cartItems.begin()+i); 
+          found = true; 
+          break; 
+          } 
     }
-   if (!found) { 
-      cout << "Item not found in cart. Nothing removed." << endl;
-   }
+      if (!found) { 
+       cout << "Item not found in cart. Nothing removed." << endl;
+      }
 }
 
 //define ModifyItem function
@@ -53,34 +53,34 @@ void ShoppingCart::ModifyItem(ItemToPurchase item) {
          found = true; 
          if (item.GetDescription() != "none") { 
             cartItems.at(i).SetDescription(item.GetDescription()); 
-         } 
-         if (item.GetPrice() != 0) { 
-            cartItems.at(i).SetPrice(item.GetQuantity()); 
-         } 
-         if (item.GetQuantity() != 0) { 
-            cartItems.at(i).SetQuantity(item.GetQuantity()); 
-         } 
-      } 
-   } 
-   if (!found) { 
-      cout << "Item not found in cart. Nothing modified." << endl; 
-   } 
-} 
+            } 
+            if (item.GetPrice() != 0) { 
+               cartItems.at(i).SetPrice(item.GetQuantity()); 
+               } 
+               if (item.GetQuantity() != 0) { 
+                  cartItems.at(i).SetQuantity(item.GetQuantity()); 
+                  } 
+               } 
+            } 
+         if (!found) { 
+          cout << "Item not found in cart. Nothing modified." << endl; 
+          } 
+    } 
           
 int ShoppingCart::GetNumItemsInCart() { 
    int totalItems = 0; 
    for (unsigned int i = 0; i < cartItems.size(); i++) { 
       totalItems += cartItems.at(i). GetQuantity(); 
-   } 
-   return totalItems;
+      } 
+      return totalItems;
 }
 
 double ShoppingCart::GetCostOfCart() {
-double totalCost = 0.0;
-for (unsigned int i = 0; i < cartItems.size(); i++) { 
-   totalCost += cartItems.at(i).GetPrice()*cartItems.at(i).GetQuantity();
-} 
-return totalCost;
+   double totalCost = 0.0;
+  for (unsigned int i = 0; i < cartItems.size(); i++) { 
+     totalCost += cartItems.at(i).GetPrice()*cartItems.at(i).GetQuantity();
+     } 
+     return totalCost;
 }
 
 void ShoppingCart:: PrintTotal() {
@@ -92,30 +92,30 @@ void ShoppingCart:: PrintTotal() {
       cout << endl;
       cout << "Total: $0" << endl; 
       } 
-   else {
-      cout << customerName << "'s Shopping Cart - " << currentDate << endl;
-      cout << "Number of Items: " << GetNumItemsInCart() << endl; 
-      cout << endl; 
-      for (unsigned int i = 0; i < cartItems.size(); i++) { 
-         cartItems.at(i). PrintItemCost(); 
-      } 
-      cout << endl; 
-      cout << "Total: $" << GetCostOfCart() << endl;
+      else {
+         cout << customerName << "'s Shopping Cart - " << currentDate << endl;
+         cout << "Number of Items: " << GetNumItemsInCart() << endl; 
+         cout << endl; 
+         for (unsigned int i = 0; i < cartItems.size(); i++) { 
+            cartItems.at(i). PrintItemCost(); 
+         } 
+         cout << endl; 
+         cout << "Total: $" << GetCostOfCart() << endl;
    } 
-} 
+ } 
             
 void ShoppingCart:: PrintDescriptions() {
    if (GetNumItemsInCart() == 0) {
       cout << "SHOPPING CART IS EMPTY" << endl; 
-      } 
-   else { 
+      } else { 
       cout << customerName << "'s Shopping Cart - " << currentDate << endl; 
-      cout << endl; cout << "Item Descriptions" << endl; 
-      for (unsigned int i = 0; i < cartItems.size(); i++) { 
+      cout << endl; 
+      cout << "Item Descriptions" << endl; 
+       for (unsigned int i = 0; i < cartItems.size(); i++) { 
           cartItems.at(i).PrintItemDescription(); 
-      } 
-   } 
-}
+          } 
+          } 
+   }
 
 
 
